525383fb16dd92b42a01b3df1b0fc2f3cc258a41
robert lee <shat0806449@gmail.com>
2022-12-28 15:01:10 +0800

    memorygameV1.0

---
+++ memorygame/Card.swift
@@ -0,0 +1,32 @@
+//
+//  Card.swift
+//  memorygame
+//
+//  Created by user on 28/12/22.
+//
+
+import UIKit
+
+struct Card {
+    var cardName:String?
+    var cardImage:UIImage?
+    var flipped:Bool? = false
+}
+
+var cards = [
+    
+    Card(cardName: "0", cardImage: UIImage(named: "0")),
+    Card(cardName: "1", cardImage: UIImage(named: "1")),
+    Card(cardName: "2", cardImage: UIImage(named: "2")),
+    Card(cardName: "3", cardImage: UIImage(named: "3")),
+    Card(cardName: "4", cardImage: UIImage(named: "4")),
+    Card(cardName: "5", cardImage: UIImage(named: "5")),
+    
+    Card(cardName: "0", cardImage: UIImage(named: "0")),
+    Card(cardName: "1", cardImage: UIImage(named: "1")),
+    Card(cardName: "2", cardImage: UIImage(named: "2")),
+    Card(cardName: "3", cardImage: UIImage(named: "3")),
+    Card(cardName: "4", cardImage: UIImage(named: "4")),
+    Card(cardName: "5", cardImage: UIImage(named: "5"))
+    
+]
